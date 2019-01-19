f4855fd10005fd6d863c2d874c6fa08b4cc48894
Jeffrey-Hargis <hargis.jeffrey@gmail.com>
2019-01-19 00:14:48 -0500

    you displayed

--- public/chatroom.js
+++ public/chatroom.js
@@ -12,38 +12,42 @@ var holder = document.getElementById("handle").placeholder;
 handle.value = holder;
 
 // Send message emit
-button.addEventListener("click", function() {
-  //emit message down the web socket to the server....sends object to server
-  socket.emit("chat", {
-    messages: message.value,
-    username: handle.value
-  });
+button.addEventListener("click", function () {
+    //emit message down the web socket to the server....sends object to server
+    socket.emit("chat", {
+        messages: message.value,
+        username: handle.value
+    });
 
-  message.value = "";
+    message.value = "";
 });
 
 message.addEventListener("keypress", () => {
-  socket.emit("typing", handle.value);
+    socket.emit("typing", handle.value);
 });
 
 // Listen for events
-socket.on("chat", function(data) {
-  feedback.innerHTML = "";
-  displayMsg(data);
+socket.on("chat", function (data) {
+    feedback.innerHTML = "";
+    displayMsg(data);
 });
 
 // listen for typing
-socket.on("typing", function(data) {
-  feedback.innerHTML = "<p><em>" + data + " is typing....</em></p>";
+socket.on("typing", function (data) {
+    feedback.innerHTML = "<p><em>" + data + " is typing....</em></p>";
 });
 
 // listen for previous messages
-socket.on("load previous notes", function(docs) {
-  docs.forEach(displayMsg);
+socket.on("load previous notes", function (docs) {
+    docs.forEach(displayMsg);
 });
 
 // display messages
 function displayMsg(data) {
-  output.innerHTML +=
-    "<p><strong>" + data.username + ": </strong>" + data.messages + "</p>";
+    // displays current user as "you" and person you are chatting with as their handle
+    const isSender = data.username === handle.value;
+    const username = isSender ? "you" : data.username
+    const className = isSender ? "myMessage" : "theirMessage"
+    output.innerHTML +=
+        "<p id=\"message\" class=\"" + className + "\"><strong>" + username + ": </strong>" + data.messages + "</p>";
 }
