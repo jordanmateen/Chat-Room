82b2f7f0dd73e3666eaa89f0e0e345c7e9ca91f0
Jeffrey-Hargis <hargis.jeffrey@gmail.com>
2019-01-18 22:32:27 -0500

    use displayMsg for incoming chats

--- public/chatroom.js
+++ public/chatroom.js
@@ -1,62 +1,49 @@
-//make connection
+// make connection
+var socket = io.connect("http://localhost:3000");
 
-var socket = io.connect('http://localhost:3000')
-
-//query DOM
-var message = document.getElementById('message');
-var handle = document.getElementById('handle');
-var button = document.getElementById('send');
-var output = document.getElementById('output');
-var feedback = document.getElementById('feedback');
+// query DOM
+var message = document.getElementById("message");
+var handle = document.getElementById("handle");
+var button = document.getElementById("send");
+var output = document.getElementById("output");
+var feedback = document.getElementById("feedback");
 var holder = document.getElementById("handle").placeholder;
 
 handle.value = holder;
 
-//Emitting Event
+// Send message emit
+button.addEventListener("click", function() {
+  //emit message down the web socket to the server....sends object to server
+  socket.emit("chat", {
+    messages: message.value,
+    username: handle.value
+  });
 
-button.addEventListener('click', function () {
-    //emit message down the web socket to the server....sends object to server
-    socket.emit('chat', {
-        message: message.value,
-        handle: handle.value
-    });
-
-    console.log('you are here');
-
-    message.value = '';
-    //handle.value = '';
+  message.value = "";
 });
 
-
-message.addEventListener('keypress', () => {
-    socket.emit('typing', handle.value);
-})
-
-
-//Lsiten for events
-socket.on('chat', function (data) {
-    feedback.innerHTML = '';
-    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
-
-    //console.log(output);
+message.addEventListener("keypress", () => {
+  socket.emit("typing", handle.value);
 });
 
-//listen for typing 
-socket.on('typing', function (data) {
-    feedback.innerHTML = '<p><em>' + data + ' is typing....</em></p>';
-
-});
-//listen for previous messages
-socket.on('load previous notes', function (docs) {
-
-    console.log(docs)
-    for (var i = docs.length - 1; i >= 0; i--) {
-        dsiplayMsgs(docs[i]);
-    }
+// Listen for events
+socket.on("chat", function(data) {
+  feedback.innerHTML = "";
+  displayMsg(data);
 });
 
+// listen for typing
+socket.on("typing", function(data) {
+  feedback.innerHTML = "<p><em>" + data + " is typing....</em></p>";
+});
 
-//display messages
-function dsiplayMsgs(data) {
-    output.innerHTML += '<p><strong>' + data.username + ': </strong>' + data.messages + '</p>';
+// listen for previous messages
+socket.on("load previous notes", function(docs) {
+  docs.forEach(displayMsg);
+});
+
+// display messages
+function displayMsg(data) {
+  output.innerHTML +=
+    "<p><strong>" + data.username + ": </strong>" + data.messages + "</p>";
 }
