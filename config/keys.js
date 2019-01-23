312f2d2081e0e550b60f40ec75e98225bd718993
Ron Cagle <ron@atlantashirt.com>
2019-01-22 19:03:48 -0500

    typo fix on config/keys

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // Google+ API keys
 
-var key  = {
+var keys  = {
     google: {
         clientID: '834400309282-56sjvsm2p8aj6dsijci9f7uf4ln2he6j.apps.googleusercontent.com',
         clientSecret: '14uji_bFvz3n78YTFpXf_Osy'
